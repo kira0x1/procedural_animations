78d462634731ea364d2833e4ac53ab293deb218c
Kira <kira.eva@protonmail.com>
2024-12-16 10:31:41 +0000

    create body segments

---
+++ Code/BodyNode.cs
@@ -0,0 +1,7 @@
+﻿namespace Kira;
+
+public class BodyNode
+{
+    public Vector3 Position { get; set; }
+    public float SegmentSize { get; set; } = 1f;
+}
--- Code/ProceduralBlobs.cs
+++ Code/ProceduralBlobs.cs
@@ -1,38 +1,90 @@
+using Kira;
+
 [Category("Kira")]
 public sealed class ProceduralBlobs : Component
 {
     [Property, Range(1, 20)]
     private int Resolution { get; set; } = 4;
 
-    [Property, Range(0f, 100f)]
-    private List<float> SegmentSize { get; set; }
 
     [Property, Range(0, 50f)]
     private float Offset { get; set; } = 5f;
+
+    [Property, Range(0, 20f)]
+    private float SegmentSize { get; set; } = 5f;
+
     private Vector3 anchorPos = Vector3.Zero;
 
-    protected override void OnValidate()
+    private List<BodyNode> nodes = new List<BodyNode>();
+
+
+    private void RefreshBodyNodes()
     {
-        var length = SegmentSize.Count;
-        if (length > Resolution)
+        nodes.Clear();
+
+        for (int i = 0; i < Resolution; i++)
         {
-            var toDel = length - Resolution;
-            var start = length - toDel;
-            SegmentSize.RemoveRange(start, toDel);
+            var node = new BodyNode();
+            node.SegmentSize = SegmentSize;
+            var pos = anchorPos;
+
+            // move to outer circle of anchor
+            // Add the radius of anchor
+            pos.y += SegmentSize * 3.2f;
+
+            // add offsets
+            
+            // offset by each segments radius
+            pos.y += i * SegmentSize * 2.2f;
+            
+            // add additional offset from offset variable
+            pos.y += i * Offset;
+
+
+            node.Position = pos;
+            // node.Position += i * Offset + 5f;
+            nodes.Add(node);
         }
     }
 
     protected override void OnUpdate()
+    {
+        DisplayGizmos();
+        RefreshBodyNodes();
+
+        using (Gizmo.Scope("nodes"))
+        {
+            foreach (BodyNode node in nodes)
+            {
+                Gizmo.Draw.Color = new Color(90f, 100f, 150f);
+                Gizmo.Draw.LineThickness = 2f;
+                Gizmo.Draw.LineCircle(node.Position, node.SegmentSize, sections: 128);
+            }
+        }
+
+        UpdateAnchorPos();
+    }
+
+    private void UpdateAnchorPos()
+    {
+        var mouse = Mouse.Position;
+        var camera = Scene.Camera;
+        var ray = camera.ScreenPixelToRay(mouse);
+
+        var tr = Scene.Trace.Ray(ray, 300f).Run();
+        anchorPos = tr.EndPosition;
+    }
+
+    private void DisplayGizmos()
     {
         var pointPos = new Vector3(0, -60f, 30f);
 
         using (Gizmo.Scope("anchor"))
         {
             Gizmo.Draw.Color = new Color(130f, 20f, 100f);
-            Gizmo.Draw.LineThickness = 6f;
-            Gizmo.Draw.LineCircle(anchorPos, 10f, sections: 128);
+            Gizmo.Draw.LineThickness = 3f;
+            Gizmo.Draw.LineCircle(anchorPos, SegmentSize * 2f, sections: 128);
         }
-
 
         using (Gizmo.Scope("point"))
         {
@@ -47,7 +99,6 @@ public sealed class ProceduralBlobs : Component
             Gizmo.Draw.Arrow(anchorPos, pointPos, 1f, 0.5f);
         }
 
-
         using (Gizmo.Scope("innerPoint"))
         {
             // var dir = anchorPos + pointPos.Normal * 10f;
@@ -58,30 +109,5 @@ public sealed class ProceduralBlobs : Component
             Gizmo.Draw.Line(anchorPos, dir);
             Gizmo.Draw.SolidCircle(dir, 1.5f, sections: 16);
         }
-
-
-        var mouse = Mouse.Position;
-        var camera = Scene.Camera;
-        var ray = camera.ScreenPixelToRay(mouse);
-
-        var tr = Scene.Trace.Ray(ray, 300f).Run();
-
-        Log.Info(tr.EndPosition);
-        anchorPos = tr.EndPosition;
-
-        // anchorPos -= Vector3.Right * 5 * Time.Delta;
-        // anchorPos += Vector3.Down * 15 * Time.Delta;
-
-        // for (int i = 0; i < Resolution; i++)
-        // {
-        //     var pos = GameObject.LocalPosition;
-        //     pos.y += Offset * i;
-        //
-        //     var innerCircle = pos;
-        //     innerCircle -= float.Cos(10f) + float.Sin(10f);
-        //
-        //     Gizmo.Draw.LineCircle(pos, 5f);
-        //     Gizmo.Draw.LineCircle(innerCircle, 1f);
-        // }
     }
 }
